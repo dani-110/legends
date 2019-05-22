6b75e3f022c94d9c4ef125935aa7b1cffd04af33
Hussain <jawwad@hussains-MacBook-Pro.local>
2019-05-22 14:03:48 +0500

    Initial removed from navigator

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -68,7 +68,7 @@ const navigator = Actions.create(
       </Scene>
       <Scene key="news" component={News} hideNavBar />
       <Scene key="poty" component={Poty} hideNavBar />
-      <Scene key="lcl" component={Lcl} hideNavBar initial />
+      <Scene key="lcl" component={Lcl} hideNavBar />
       <Scene key="profile" component={Profile} hideNavBar />
     </Drawer>
   </Stack>
