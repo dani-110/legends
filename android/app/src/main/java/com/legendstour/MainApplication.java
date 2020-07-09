4e3e378daa85c8086778a6fc2bf0a89f6913cb12
osamalivewireapps <osama@livewireapps.com>
2020-07-09 11:01:57 +0500

    on 9th july

--- android/app/src/main/java/com/legendstour/MainApplication.java
+++ android/app/src/main/java/com/legendstour/MainApplication.java
@@ -3,6 +3,9 @@ package com.legendstour;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.clipsub.rnbottomsheet.RNBottomSheetPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -30,6 +33,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
+            new ReanimatedPackage(),
+            new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
             new RNBottomSheetPackage(),
             new PickerPackage(),
